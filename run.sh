e3cebbba6d2d0631d201ebd259a8f83164feb135
mli <muli@cs.cmu.edu>
2018-11-24 04:12:01 +0000

    remove

--- run.sh
+++
@@ -1,9 +0,0 @@
-# source activate mxnet-docs
-
-rm -rf build/_build/
-make html
-
-aws s3 sync --delete build/_build/html/ s3://mxnet-new-docs/ --acl public-read
-cd build/_build/html/ && python -m http.server
-
-sphinx-autogen *rst -t ../../templates/
